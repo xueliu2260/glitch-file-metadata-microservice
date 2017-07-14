18180b1097334ab62f5b9c0cb2b5481555693b07
xueliu2260 <xueliu2260@gmail.com>
2017-07-13 18:10:29 -0700

    Update client.js

--- public/client.js
+++ public/client.js
@@ -6,7 +6,31 @@
 
 $(function() {
   console.log('hello world :o');
-  
-  
+//   $.get('/fileupload', function(dreams) {
+//     dreams.forEach(function(dream) {
+//       $('<li></li>').text(dream).appendTo('ul#fileupload');
+//     });
+//   });
+
+  $('form').submit(function(event) {
+    event.preventDefault();
+    
+    var formData = new FormData();
+    formData.append("file", $("#image-file")[0].files[0]);
+    
+    $.ajax({
+        url: '/fileupload',
+        data: formData,
+        contentType: false,
+        processData: false,
+        type: 'POST',
+        success: function(data){
+            $('<p></p>').text(data).appendTo('p#fileupload');;
+            $('input').val('');
+            $('input').focus();
+        }
+    });
+    
+});
 
 });
